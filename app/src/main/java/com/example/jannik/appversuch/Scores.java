250b51c3aba08500f9736f4f4561a2a8b26dc465
JaBuck <buja14hz@.hj.se>
2015-02-13 18:08:40 +0100

    Main Screen Push

--- app/src/main/java/com/example/jannik/appversuch/Scores.java
+++ app/src/main/java/com/example/jannik/appversuch/Scores.java
@@ -1,26 +1,24 @@
 package com.example.jannik.appversuch;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class Scores extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_scores);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_scores, menu);
         return true;
     }
 
@@ -37,17 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void newGame (View view){
-        Intent intent = new Intent(this, QuizScreen.class);
-    }
-
-    public void settings (View view){
-        Intent intent = new Intent(this, Settings.class);
-    }
-
-    public void scores (View view){
-        Intent intent = new Intent(this, Scores.class);
     }
 }

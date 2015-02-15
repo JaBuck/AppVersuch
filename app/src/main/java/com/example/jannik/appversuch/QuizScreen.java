8ab971a6662e0245835dada86c9264485fe54e42
JaBuck <buja14hz@.hj.se>
2015-02-15 14:44:38 +0100

    Test Intent

--- app/src/main/java/com/example/jannik/appversuch/QuizScreen.java
+++ app/src/main/java/com/example/jannik/appversuch/QuizScreen.java
@@ -50,7 +50,7 @@ public class QuizScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_quizScreen);
+        setContentView(R.layout.activity_quizscreen);
 
         final View controlsView = findViewById(R.id.fullscreen_content_controls);
         final View contentView = findViewById(R.id.fullscreen_content);
@@ -112,7 +112,7 @@ public class QuizScreen extends Activity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
--- app/src/main/java/com/example/jannik/appversuch/TemporaryScreen.java
+++ app/src/main/java/com/example/jannik/appversuch/TemporaryScreen.java
@@ -6,32 +6,41 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class TemporaryScreen extends ActionBarActivity {
 
-    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
-
-    public void newGame (View view){
+    public void temp_button (View view){
         Intent intent = new Intent(this, QuizScreen.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+       //Get message from intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        //Create Text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(49);
+        textView.setText(message);
+
+        //Set text view as in activity layout
+
+        setContentView(textView);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_temporary_screen, menu);
         return true;
     }
 
@@ -48,17 +57,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-
-
-    public void settings (View view){
-        Intent intent = new Intent(this, Settings.class);
-        startActivity(intent);
-    }
-
-    public void scores (View view){
-        Intent intent = new Intent(this, Scores.class);
-        startActivity(intent);
     }
 }
